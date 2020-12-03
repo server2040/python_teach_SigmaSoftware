f7bccebe3b9490ca05dc7e29418e4908e9c09d06
server2040 <server2040@gmail.com>
2020-12-03 14:56:17 +0200

    Update teach3_z1_1.py

--- teach3/teach3_z1_1.py
+++ teach3/teach3_z1_1.py
@@ -1,1 +1,4 @@
+print('Please input value')
 try:
+ print('result: '+ str( (lambda x, y: x / y if y != 0 else None) ( float(input(' x: ')),float( input(' y: '))) )) 
+ print('Error: input value is not number')
